0150352e9dc682f592827b81e2eb613d73aeaa5d
David <iceterminal@gmail.com>
2017-11-15 19:33:28 -0600

    Update wk3

---
+++ wk3/create_a_list_of_one_element.py
@@ -0,0 +1,8 @@
+# First, we need to set up a variable.
+# Replace the ? with how many chickens you think have crossed the road today.
+number_of_chickens = 3
+
+# Set up a new list `my_list` containing the number_of_chickens variable! Wooo!
+my_list = [number_of_chickens]
+
+length_of_my_list = len(my_list)  # Use the `len` function to count how many items `my_list` has.
---
+++ wk3/create_a_list_with_three_elements.py
@@ -0,0 +1,7 @@
+# Enter 3 words to describe pizza
+word1 = 'cheesy'
+word2 = 'chewy'
+word3 = 'saucy'
+
+# Now make a list containing each of these words and store in a variable called `pizza_time`.
+pizza_time = [word1, word2, word3]
