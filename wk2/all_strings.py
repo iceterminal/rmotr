2c9061bf8879bbc2ec188b5597c5b4e2b0c3b683
David <iceterminal@gmail.com>
2017-11-16 19:12:31 -0600

    Update wk2 testing

--- wk2/all_strings.py
+++ wk2/all_strings.py
@@ -10,3 +10,19 @@ def all_strings(a, b, c):
 print(type('hello'))
 print(type(True))
 print(type(123))
+
+
+
+
+#Test With None - RUN TEST
+def test_with_none():
+    assert all_strings('hello', None, 'Python') == False
+# Test With Empty String - RUN TEST
+def test_with_empty_string():
+    assert all_strings('', 'rmotr', 'Python') == True
+# Test With A Number - RUN TEST
+def test_with_a_number():
+    assert all_strings(3, 'rmotr', 'Python') == False
+ #Test With All Strings - RUN TEST
+def test_with_all_strings():
+    assert all_strings('j', 'rmotr', 'Python') == True
--- wk2/knock_3_times.py
+++ wk2/knock_3_times.py
@@ -3,3 +3,18 @@ def knock_three_times():
     while knock < 4:
         print("Knock Knock")
         knock = knock + 1
+
+
+
+
+#Test Knock Three Times - RUN TEST
+def test_knock_three_times():
+    with CaptureOutput() as output:
+        knock_three_times()
+
+    assert len(output) == 3
+    assert output == [
+        'Knock Knock',
+        'Knock Knock',
+        'Knock Knock'
+    ]
